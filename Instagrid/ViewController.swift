8709e4edc1ae8ebe695ed026b5a276d3e8db14f2
Marine Bernard <bernard-marine@hotmail.com>
2020-05-21 17:05:59 +0200

    create actions

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - outlets
+    
+    @IBOutlet var plusButtons: [UIButton]!
+    @IBOutlet var layoutButtons: [UIButton]!
+    
+    // MARK: - view life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - actions
+    
+    @IBAction func layoutButtonTapped(_ sender: UIButton) {
+        for button in layoutButtons {
+            button.isSelected = false
+        }
+        sender.isSelected = true
+        switch sender.tag {
+        case 0:
+            plusButtons[1].isHidden = true
+            plusButtons[3].isHidden = false
+        case 1:
+            plusButtons[1].isHidden = false
+            plusButtons[3].isHidden = true
+        case 2:
+            plusButtons[1].isHidden = false
+            plusButtons[3].isHidden = false
+        default: break
+        }
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        print("plus")
+    }
 }
